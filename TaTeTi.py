bf91c253d9a1d1dde737d2d6af842c8e512e9678
diego-villarroel <diego.n.villarroel@gmail.com>
2020-09-20 18:48:38 -0300

    6 movimientos por Jugador

--- TaTeTi.py
+++ TaTeTi.py
@@ -1,6 +1,34 @@
 #coding: utf8
 import os
 
+#TABLERO
+columna1_TaTeTi = ['_', '_', '_']
+columna2_TaTeTi = ['_', '_', '_']
+columna3_TaTeTi = ['_', '_', '_']
+filas_TaTeTi = [columna1_TaTeTi, columna2_TaTeTi, columna3_TaTeTi]
+
+def mostrar_tablero():
+    print("|{}|{}|{}|".format(columna1_TaTeTi[0], columna1_TaTeTi[1], columna1_TaTeTi[2]))
+    print("|{}|{}|{}|".format(columna2_TaTeTi[0], columna2_TaTeTi[1], columna2_TaTeTi[2]))
+    print("|{}|{}|{}|".format(columna3_TaTeTi[0], columna3_TaTeTi[1], columna3_TaTeTi[2]))
+
+#JUGADOR 1
+def player1():
+    seleccionPlayer1_columna = int(input("Selecciona una columna: ") - 1)
+    seleccionPlayer1_fila = int(input("Selecciona una fila: ") - 1)
+
+    player1_Fila = filas_TaTeTi[seleccionPlayer1_fila]
+    player1_Columna = player1_Fila.insert(seleccionPlayer1_columna, 'X')
+
+#JUGADOR 2
+def player2():
+    seleccionPlayer2_columna = int(input("Selecciona una columna: ") - 1)
+    seleccionPlayer2_fila = int(input("Selecciona una fila: ") - 1)
+
+    player2_Fila = filas_TaTeTi[seleccionPlayer2_fila]
+    player2_Columna = player2_Fila.insert(seleccionPlayer2_columna, 'O')
+
+#INTRODUCCION E INSTRUCCIONES
 print("BIENVENIDOS A CLASIC TA-TE-TI - 2 Jugadores")
 print("")
 print("Hecho por Diego Villarroel")
@@ -26,6 +54,7 @@ raw_input("Presiona Enter para continuar")
 
 os.system('clear')
 
+#GAMEPLAY
 print("Comienza el Jugador 1!")
 print("")
 print("|_|_|_|")
@@ -34,20 +63,76 @@ print("|_|_|_|")
 print("")
 raw_input("Presiona Enter para continuar...")
 print("")
-
-#TABLERO
-columna1_TaTeTi = ['_', '_', '_']
-columna2_TaTeTi = ['_', '_', '_']
-columna3_TaTeTi = ['_', '_', '_']
-filas_TaTeTi = [columna1_TaTeTi, columna2_TaTeTi, columna3_TaTeTi]
-
-#JUGADOR 1
-seleccionPlayer1_columna = int(input("Selecciona una columna: ") - 1)
-seleccionPlayer1_fila = int(input("Selecciona una fila: ") - 2)
-
-player1_Fila = filas_TaTeTi[seleccionPlayer1_fila]
-player1_Columna = player1_Fila.insert(seleccionPlayer1_columna, 'X')
-
-print("|{}|{}|{}|".format(columna1_TaTeTi[0], columna1_TaTeTi[1], columna1_TaTeTi[2]))
-print("|{}|{}|{}|".format(columna2_TaTeTi[0], columna2_TaTeTi[1], columna2_TaTeTi[2]))
-print("|{}|{}|{}|".format(columna3_TaTeTi[0], columna3_TaTeTi[1], columna3_TaTeTi[2]))
+player1()
+os.system('clear')
+print("Ahora es el turno del Jugador 2!")
+print("")
+mostrar_tablero()
+print("")
+player2()
+os.system('clear')
+print("Ahora es el turno del Jugador 1!")
+print("")
+mostrar_tablero()
+print("")
+player1()
+os.system('clear')
+print("Ahora es el turno del Jugador 2!")
+print("")
+mostrar_tablero()
+print("")
+player2()
+os.system('clear')
+print("Ahora es el turno del Jugador 1!")
+print("")
+mostrar_tablero()
+print("")
+player1()
+os.system('clear')
+print("Ahora es el turno del Jugador 2!")
+print("")
+mostrar_tablero()
+print("")
+player2()
+os.system('clear')
+print("Ahora es el turno del Jugador 1!")
+print("")
+mostrar_tablero()
+print("")
+player1()
+os.system('clear')
+print("Ahora es el turno del Jugador 2!")
+print("")
+mostrar_tablero()
+print("")
+player2()
+os.system('clear')
+print("Ahora es el turno del Jugador 1!")
+print("")
+mostrar_tablero()
+print("")
+player1()
+os.system('clear')
+print("Ahora es el turno del Jugador 2!")
+print("")
+mostrar_tablero()
+print("")
+player2()
+os.system('clear')
+print("Ahora es el turno del Jugador 1!")
+print("")
+mostrar_tablero()
+print("")
+player1()
+os.system('clear')
+print("Ahora es el turno del Jugador 2!")
+print("")
+mostrar_tablero()
+print("")
+player2()
+os.system('clear')
+print("Ahora es el turno del Jugador 1!")
+print("")
+mostrar_tablero()
+print("")
+player1()
